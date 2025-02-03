09f015762a5f0c1bd7d9327ee004bae3beb110ae
DK0280705 <dekarismanpermana@gmail.com>
2025-02-03 18:08:57 +0900

    Better voice tick handler

--- src/state.rs
+++ src/state.rs
@@ -24,26 +24,32 @@ impl VoiceEventHandler for VoiceHandler {
     async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
         match ctx {
             EventContext::VoiceTick(tick) => {
-                let speakers_len: i32 = tick.speaking.len() as i32;
-                if speakers_len == 0 {
-                    return None;
-                }
+                // This verbosity is educational purpose.
+                // The length of the voice samples:
+                // num_samples = duration_s * sample_rate_Hz * num_channels
+                const SAMPLE_RATE: usize = 48; // kHz
+                const DURATION_MS: usize = 20; // voice data duration in ms
+                const CHANNEL_SIZE: usize = 2; // 2 channel audio
+                const SAMPLE_LENGTH: usize = SAMPLE_RATE * DURATION_MS * CHANNEL_SIZE;
 
-                let mut voice_data: Vec<i32> = Vec::new();
+                let num_speakers = tick.speaking.len() as i32;
+
+                let mut voice_data = vec![0i32; SAMPLE_LENGTH];
+                
                 for (_, data) in &tick.speaking {
                     let decoded_voice = data.decoded_voice.as_ref().unwrap();
-                    if voice_data.len() < decoded_voice.len() {
-                        voice_data.resize(decoded_voice.len(), 0);
-                    }
                     for (sample1, sample2) in voice_data.iter_mut().zip(decoded_voice.iter()) {
                         *sample1 += *sample2 as i32;
                     }
                 }
+
                 let transformed_data: Vec<u8> = voice_data.into_iter()
-                    .flat_map(|data| ((data / speakers_len) as i16).to_le_bytes())
+                    .flat_map(|data| ((data / num_speakers) as i16).to_le_bytes())
                     .collect();
-                let mut file = self.file.lock().await;
-                let _ = file.write_all(&transformed_data);
+                {
+                    let mut file = self.file.lock().await;
+                    let _ = file.write_all(&transformed_data);
+                }
             }
             _ => { return None; },
         }
